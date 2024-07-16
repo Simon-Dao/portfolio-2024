75e977f2b8ea6d1a27c0b2d6232bf3102eae5e6c
Simon Dao <SimonNDao12@gmail.com>
2024-07-16 00:37:24 -0700

    fixed mobile for project i think

--- app/(no footer)/projects/page.tsx
+++ app/(no footer)/projects/page.tsx
@@ -19,7 +19,7 @@ function Projects() {
   return (
 
     <div className='px-0 lg:px-48 pb-64 pt-52'>
-      <div className='px-24'>
+      <div className='px-3 sm:px-24'>
         <section className='flex justify-center'>
           <div style={{ marginTop: "" }} className=" text-title m-0 select-none flex flex-col items-start">
             <span className='text-6xl sm:text-8xl'>
@@ -27,7 +27,7 @@ function Projects() {
             </span>
           </div>
           <Image
-            className='ml-20 self-end mb-12 text-pm'
+            className='ml-20 self-end mb-12 text-pm hidden sm:block'
             priority={false}
             src={ArrowSVG}
             alt=""
@@ -35,11 +35,11 @@ function Projects() {
         </section>
 
         <section className='w-full flex mt-14 pb-5'>
-          <div className='flex overflow-hidden'>
-            <GenericButton className='w-36 mr-3 flex justify-center items-center border-solid border-gray-500 rounded-full cursor-pointer h-20'>
+          <div className='flex flex-col sm:flex-row overflow-hidden w-full'>
+            <GenericButton className='my-3 self-center sm:self-auto sm:my-0 w-36 mr-3 flex justify-center items-center border-solid border-gray-500 rounded-full cursor-pointer h-20'>
               <a href="https://github.com/Simon-Dao">Github</a>
             </GenericButton>
-            <GenericButton className='w-36 mr-3 flex justify-center items-center border-solid border-gray-500 rounded-full cursor-pointer h-20'>
+            <GenericButton className='my-3 self-center sm:self-auto sm:my-0 w-36 mr-3 flex justify-center items-center border-solid border-gray-500 rounded-full cursor-pointer h-20'>
               <TransitionLink href="/resume" label='Resume' hoverAnimation={true} />
             </GenericButton>
           </div>
