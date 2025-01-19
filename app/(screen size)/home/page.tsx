9d6cc8d69e9a051b7f607075502f6d7bba9a49a8
Simon Nguyen Dao <55553313+Simon-Dao@users.noreply.github.com>
2025-01-19 00:37:23 -0800

    changed pages for husky100 award

--- app/(screen size)/home/page.tsx
+++ app/(screen size)/home/page.tsx
@@ -1,100 +1,138 @@
-'use client'
-import React, { useEffect, useRef, useState } from 'react'
-import DavidScene from '@/components/davidHead';
-import gsap from 'gsap'
+"use client";
+import React, { useEffect, useRef, useState } from "react";
+import DavidScene from "@/components/davidHead";
+import gsap from "gsap";
+import Image from "next/image";
+import { div } from "three/examples/jsm/nodes/Nodes.js";
+import Magnetic from "@/components/magnetic";
+import TransitionLink from "@/components/transitionLink";
 
 function Home() {
-
   return (
     <>
       <NormalView />
       <MobileView />
     </>
-  )
+  );
 }
 
 function MobileView() {
-
-  const containerRef = useRef(null)
+  const containerRef = useRef(null);
   const c1 = useRef<HTMLDivElement>(null);
   const c2 = useRef<HTMLDivElement>(null);
   const titleRef = useRef<HTMLDivElement>(null);
-  const [carouselWidth, setCarouselWidth] = useState<string>('500px');
+  const [carouselWidth, setCarouselWidth] = useState<string>("500px");
 
   const handleTitleResize = () => {
     const titleWidth = titleRef.current?.scrollWidth || 0;
     setCarouselWidth(titleWidth + "px");
-  }
+  };
 
   useEffect(() => {
-
     const t1 = gsap.timeline({ repeat: -1 });
     const contentWidth = c1.current?.scrollWidth || 0;
     const titleWidth = titleRef.current?.scrollWidth || 0;
 
-    setCarouselWidth(titleWidth + 'px')
+    setCarouselWidth(titleWidth + "px");
 
     t1.set(c1.current, {
-      translateX: 0
+      translateX: 0,
     })
       .to(c1.current, {
         translateX: -contentWidth,
-        ease: 'linear',
+        ease: "linear",
         duration: 10, // Adjust the duration to your liking
-      }).set(c1.current, { translateX: contentWidth });
+      })
+      .set(c1.current, { translateX: contentWidth });
 
     const t2 = gsap.timeline({ repeat: -1 });
     t2.set(c2.current, {
-      translateX: 0
+      translateX: 0,
     })
       .to(c2.current, {
         translateX: -contentWidth,
-        ease: 'linear',
+        ease: "linear",
         duration: 10, // Adjust the duration to your liking
-      }).set(c2.current, { translateX: contentWidth });
-
+      })
+      .set(c2.current, { translateX: contentWidth });
   }, []);
 
   return (
-    <div className='sm:hidden grow flex flex-col justify-center items-center bg-black'>
-
-      <h1 ref={titleRef} onResize={handleTitleResize} className="text-6xl m-0 select-none pt-16">Simon Dao</h1>
-      <div className='overflow-x-hidden flex h-20' style={{ width: carouselWidth }}>
-
-        <div ref={c1} className="text-lg xs:text-sd m-0 select-none text-nowrap flex">
-          <span className='px-3 text-sm xs:text-xl '>Developer</span>
-          <span className='px-3 text-sm xs:text-xl '>Designer</span>
-          <span className='px-3 text-sm xs:text-xl '>Problem-Solver</span>
+    <div className="sm:hidden grow flex flex-col justify-center items-center bg-black">
+      <h1
+        ref={titleRef}
+        onResize={handleTitleResize}
+        className="text-6xl m-0 select-none pt-16"
+      >
+        Simon Dao
+      </h1>
+      <div
+        className="overflow-x-hidden flex h-20"
+        style={{ width: carouselWidth }}
+      >
+        <div
+          ref={c1}
+          className="text-lg xs:text-sd m-0 select-none text-nowrap flex"
+        >
+          <span className="px-3 text-sm xs:text-xl ">Developer</span>
+          <span className="px-3 text-sm xs:text-xl ">Designer</span>
+          <span className="px-3 text-sm xs:text-xl ">Problem-Solver</span>
         </div>
-        <div ref={c2} className="text-lg xs:text-sd m-0 select-none text-nowrap flex">
-          <span className='px-3 text-sm xs:text-xl '>Developer</span>
-          <span className='px-3 text-sm xs:text-xl '>Designer</span>
-          <span className='px-3 text-sm xs:text-xl '>Problem-Solver</span>
+        <div
+          ref={c2}
+          className="text-lg xs:text-sd m-0 select-none text-nowrap flex"
+        >
+          <span className="px-3 text-sm xs:text-xl ">Developer</span>
+          <span className="px-3 text-sm xs:text-xl ">Designer</span>
+          <span className="px-3 text-sm xs:text-xl ">Problem-Solver</span>
         </div>
       </div>
 
-      <div className='grow w-3/5'>
-        <DavidScene />
+      <div className="grow w-3/5">
+        {/* <DavidScene /> */}
+        <Image
+          src="/public/club_fair.jpg"
+          alt="Description of image"
+          objectFit="cover"
+          layout="fill"
+        ></Image>
       </div>
-
     </div>
-  )
+  );
 }
 
 function NormalView() {
-  
   return (
     <div className="grow hidden sm:flex flex-row justify-center items-center bg-black">
-      <div className='flex flex-col-reverse md:flex-row h-4/5 w-4/5'>
-        <div className='flex grow flex-col self-center w-1/2 items-center justify-center'>
+      <div className="flex flex-col-reverse md:flex-row h-4/5 w-4/5">
+        <div className="flex grow flex-col self-center w-1/2 items-center justify-center">
           <h1 className="text-title text-white m-0 select-none">Simon Dao</h1>
+
+          <Magnetic>
+            <div className="grow flex justify-center items-center font-bold">
+              <div className="bg-tt rounded-md px-10 py-5">
+                <TransitionLink
+                  text={"text-lg"}
+                  hoverAnimation={false}
+                  href="/about"
+                  label={"Learn More"}
+                />
+              </div>
+            </div>
+          </Magnetic>
         </div>
-        <div className='grow'>
-          <DavidScene />
+        <div className="grow flex items-center justify-center">
+          <Image
+            className="rounded-full"
+            src="/simon.jpg"
+            alt="Description of image"
+            width={500}
+            height={500}
+          ></Image>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- components/header.tsx
+++ components/header.tsx
@@ -1,9 +1,9 @@
-"use client"
-import React, { useRef, useState } from 'react'
-import TransitionLink from './transitionLink'
-import Magnetic from './magnetic'
-import DotAnim from './dotAnim'
-import gsap from 'gsap'
+"use client";
+import React, { useRef, useState } from "react";
+import TransitionLink from "./transitionLink";
+import Magnetic from "./magnetic";
+import DotAnim from "./dotAnim";
+import gsap from "gsap";
 
 function header() {
   return (
@@ -11,88 +11,112 @@ function header() {
       <NormalView />
       <MobileView />
     </>
-  )
+  );
 }
 
 function NormalView() {
   return (
-    <nav className='hidden sm:flex pt-14 sm:px-16 text-white'>
-      <div className='lg:grow flex justify-start'>
+    <nav className="hidden sm:flex pt-14 sm:px-16 text-white">
+      <div className="lg:grow flex justify-start">
         <Magnetic>
-          <div className='sm:ml-20 p-2'>
+          <div className="sm:ml-20 p-2">
             <DotAnim>
-              <TransitionLink href="/home" label="Home" hoverAnimation={true} />
+              <TransitionLink
+                href="/home"
+                label="Simon Dao"
+                hoverAnimation={true}
+              />
             </DotAnim>
           </div>
         </Magnetic>
       </div>
-      <div className='grow flex justify-end'>
-      <Magnetic>
-          <div className='sm:ml-20 p-2'>
+      <div className="grow flex justify-end">
+        <Magnetic>
+          <div className="sm:ml-20 p-2">
             <DotAnim>
-              <TransitionLink href="/about" label="About" hoverAnimation={true} />
+              <TransitionLink
+                href="/about"
+                label="About"
+                hoverAnimation={true}
+              />
             </DotAnim>
           </div>
         </Magnetic>
         <Magnetic>
-          <div className='sm:ml-20 p-2'>
+          <div className="sm:ml-20 p-2">
             <DotAnim>
-              <TransitionLink href="/resume" label="Resume" hoverAnimation={true} />
+              <TransitionLink
+                href="/experiences"
+                label="Experiences"
+                hoverAnimation={true}
+              />
             </DotAnim>
           </div>
         </Magnetic>
         <Magnetic>
-          <div className='sm:ml-20 p-2'>
+          <div className="sm:ml-20 p-2">
             <DotAnim>
-              <TransitionLink href="/projects" label="Projects" hoverAnimation={true} />
+              <TransitionLink
+                href="/projects"
+                label="Projects"
+                hoverAnimation={true}
+              />
             </DotAnim>
           </div>
         </Magnetic>
         <Magnetic>
-          <div className='sm:ml-20 p-2'>
+          <div className="sm:ml-20 p-2">
             <DotAnim>
-              <TransitionLink href="/contact" label="Contact" hoverAnimation={true} />
+              <TransitionLink
+                href="/contact"
+                label="Contact"
+                hoverAnimation={true}
+              />
             </DotAnim>
           </div>
         </Magnetic>
-
       </div>
-    </nav >
-  )
+    </nav>
+  );
 }
 
 function MobileView() {
-
   const [open, setOpen] = useState<boolean>(false);
   const navRef = useRef<HTMLDivElement>(null);
 
   const openNav = () => {
-    gsap.fromTo(navRef.current, {
-      translateX: '100vw',
-      display: 'block',
-    }, {
-      translateX: '0px',
-      duration: 0.3
-    });
-
-  }
+    gsap.fromTo(
+      navRef.current,
+      {
+        translateX: "100vw",
+        display: "block",
+      },
+      {
+        translateX: "0px",
+        duration: 0.3,
+      }
+    );
+  };
 
   const closeNav = () => {
     const tl = gsap.timeline();
 
-    tl.fromTo(navRef.current, {
-      translateX: '0px',
-      display: 'block'
-    }, {
-      translateX: '100vw',
-      duration: 0.3
-    }).to(navRef.current, {
-      display: 'none',
+    tl.fromTo(
+      navRef.current,
+      {
+        translateX: "0px",
+        display: "block",
+      },
+      {
+        translateX: "100vw",
+        duration: 0.3,
+      }
+    ).to(navRef.current, {
+      display: "none",
     });
-  }
+  };
 
   const toggleNav = () => {
-
     setOpen(!open);
 
     if (open) {
@@ -100,43 +124,82 @@ function MobileView() {
     } else {
       closeNav();
     }
-  }
+  };
 
   return (
-    <div className='sm:hidden bg-transparent text-white'>
-
-      <button onClick={toggleNav} className='rounded-full z-40 bg-tt w-10 h-10 fixed top-2 right-2'>
+    <div className="sm:hidden bg-transparent text-white">
+      <button
+        onClick={toggleNav}
+        className="rounded-full z-40 bg-tt w-10 h-10 fixed top-2 right-2"
+      >
         =
       </button>
-      {open && <nav ref={navRef} className={'flex flex-col z-30 fixed h-screen w-full bg-pt justify-center items-center'}>
-        <div className='sm:ml-20 p-2'>
-          <DotAnim>
-            <TransitionLink onClick={closeNav} text='text-3xl' href="/home" label="Home" hoverAnimation={true} />
-          </DotAnim>
-        </div>
-        <div className='sm:ml-20 p-2'>
-          <DotAnim>
-            <TransitionLink onClick={closeNav} text='text-3xl' href="/about" label="About" hoverAnimation={true} />
-          </DotAnim>
-        </div>
-        <div className='sm:ml-20 p-2'>
-          <DotAnim>
-            <TransitionLink onClick={closeNav} text='text-3xl' href="/resume" label="Resume" hoverAnimation={true} />
-          </DotAnim>
-        </div>
-        <div className='sm:ml-20 p-2'>
-          <DotAnim>
-            <TransitionLink onClick={closeNav} text='text-3xl' href="/projects" label="Projects" hoverAnimation={true} />
-          </DotAnim>
-        </div>
-        <div className='sm:ml-20 p-2'>
-          <DotAnim>
-            <TransitionLink onClick={closeNav} text='text-3xl' href="/contact" label="Contact" hoverAnimation={true} />
-          </DotAnim>
-        </div>
-      </nav>}
+      {open && (
+        <nav
+          ref={navRef}
+          className={
+            "flex flex-col z-30 fixed h-screen w-full bg-pt justify-center items-center"
+          }
+        >
+          <div className="sm:ml-20 p-2">
+            <DotAnim>
+              <TransitionLink
+                onClick={closeNav}
+                text="text-3xl"
+                href="/home"
+                label="Home"
+                hoverAnimation={true}
+              />
+            </DotAnim>
+          </div>
+          <div className="sm:ml-20 p-2">
+            <DotAnim>
+              <TransitionLink
+                onClick={closeNav}
+                text="text-3xl"
+                href="/about"
+                label="About"
+                hoverAnimation={true}
+              />
+            </DotAnim>
+          </div>
+          <div className="sm:ml-20 p-2">
+            <DotAnim>
+              <TransitionLink
+                onClick={closeNav}
+                text="text-3xl"
+                href="/resume"
+                label="Resume"
+                hoverAnimation={true}
+              />
+            </DotAnim>
+          </div>
+          <div className="sm:ml-20 p-2">
+            <DotAnim>
+              <TransitionLink
+                onClick={closeNav}
+                text="text-3xl"
+                href="/projects"
+                label="Projects"
+                hoverAnimation={true}
+              />
+            </DotAnim>
+          </div>
+          <div className="sm:ml-20 p-2">
+            <DotAnim>
+              <TransitionLink
+                onClick={closeNav}
+                text="text-3xl"
+                href="/contact"
+                label="Contact"
+                hoverAnimation={true}
+              />
+            </DotAnim>
+          </div>
+        </nav>
+      )}
     </div>
-  )
+  );
 }
 
-export default header
+export default header;
